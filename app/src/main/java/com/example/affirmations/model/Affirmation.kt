8cda0aa52aecece9856ecc82a15a7ceacb0bc0fd
raymondlam88 <lamjy-pm18@student.tarc.edu.my>
2021-02-03 12:00:45 +0800

    Second updates.

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,3 +1,6 @@
 package com.example.affirmations.model
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
-data class Affirmation (val stringResourceId:Int)
+data class Affirmation (@StringRes val stringResourceId: Int,
+                        @DrawableRes val imageResourceId: Int)
